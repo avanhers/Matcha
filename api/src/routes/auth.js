27122e810843f3217c877af505e6e705ecd260df
Jules JAEGLE <jjaegle@e1r5p12.42.fr>
2020-09-24 13:32:03 +0200

    removing db

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/AuthController');
 
-router.get('/', controller.getUser);
 router.post('/inscription', controller.inscription);
 router.get('/confirmation/:infos', controller.confirmation);
 
