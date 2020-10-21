46b74a84a576b59a8a4fd0a6bbbbadbee8d71ebc
Jules JAEGLE <jjaegle@e1r5p8.42.fr>
2020-10-21 16:48:35 +0200

    test socket

--- api/socket.js
+++ api/socket.js
@@ -1,5 +1,16 @@
 let io = require("socket.io");
 
+const users {
+  user
+}
+
+const socketAuth = (socket, next) => {
+  const token = socket.handshake.query.token;
+
+  console.log(token);
+  return next(new Error("Noting Defined"));
+};
+
 const socketConnection = (socket) => {
   socket.emit("message", { message: "Hey!" });
 };
@@ -8,6 +19,7 @@ exports.startIo = function startIo(server) {
   io = io.listen(server);
 
   const test = io.of("/test");
+  test.use(socketAuth);
   test.on("connection", socketConnection);
 
   return io;
