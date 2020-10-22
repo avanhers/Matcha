45cd1d6b211063d18532d66d00158bae7c1f3722
Jules JAEGLE <jjaegle@e1r5p8.42.fr>
2020-10-22 11:45:24 +0200

    socket.io first try

--- api/socket.js
+++ api/socket.js
@@ -1,18 +1,46 @@
 let io = require("socket.io");
+const tokenManager = require("./src/lib/jwt");
 
-const users {
-  user
-}
+const sockets = {};
 
 const socketAuth = (socket, next) => {
   const token = socket.handshake.query.token;
+  const username = tokenManager.getUser(token);
 
-  console.log(token);
+  if (username) {
+    sockets[username] = socket.id;
+    console.log(`SOCKETIO: ${username} connected`);
+    return next();
+  }
   return next(new Error("Noting Defined"));
 };
 
 const socketConnection = (socket) => {
+  const username = Object.keys(sockets).find(
+    (key) => sockets[key] === socket.id
+  );
   socket.emit("message", { message: "Hey!" });
+
+  socket.on("disconnect", () => {
+    console.log(`SOCKETIO: ${username} disconnected`);
+    //delete socket
+  });
+
+  socket.on("notification", (data) => {
+    const { target, type } = data;
+    const socket_target = sockets[target];
+
+    if (socket_target) {
+      console.log(`${target} joinable at ${socket_target}`);
+      if (type === "like") {
+        io.to(socket_target).emit("notification", {
+          type: "like",
+          from: username,
+        });
+      }
+    }
+    //ecriture en bdd
+  });
 };
 
 exports.startIo = function startIo(server) {
